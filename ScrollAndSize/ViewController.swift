e3b35ec6b032a0e4b5db6a0fbd24a0eb648a872a
Sergey Garbalinskiy <garbalinskiy@me.com>
2018-07-16 22:55:26 +0300

    Initial Commit

---
+++ ScrollAndSize/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScrollAndSize
+//
+//  Created by Sergey Garbalinskiy on 7/16/18.
+//  Copyright © 2018 Sergey Garbalinskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
